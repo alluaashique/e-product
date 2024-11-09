232c61d58ced1e3dd2d367ab81db4e9e2f4b9f60
User <alluaashique007@gmail.com>
2024-11-09 17:59:52 +0530

    specs

---
+++ database/migrations/2024_11_09_122742_product_specs.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_specs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('category_id')->nullable()->constrained('product_categories')->cascadeOnDelete();
+            $table->foreignId('brand_id')->nullable()->constrained('brands')->cascadeOnDelete();            
+            $table->foreignId('product_names_id')->nullable()->constrained('product_names')->cascadeOnDelete();
+            $table->foreignId('product_id')->nullable()->constrained('products')->cascadeOnDelete();
+            $table->string('specification')->nullable();
+            $table->tinyInteger('is_active')->default(1)->comment('1:active, 0:inactive');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_specs');
+    }
+};
