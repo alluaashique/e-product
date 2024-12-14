ee78d723df29e66d7875f3d42d96e64a1c8fa53e
User <alluaashique007@gmail.com>
2024-12-14 16:35:07 +0530

    change

--- resources/views/user/product/product.blade.php
+++ resources/views/user/product/product.blade.php
@@ -142,7 +142,7 @@
                                              <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">{{$product->category->name}}</div>
                                              <div class="p-4 border border-secondary border-top-0 rounded-bottom">
                                                  <h4>{{$product->name}}</h4>
-                                                 <p>{{$product->description}}</p>
+                                                 <p>{{$product->short_description}}</p>
                                                  <div class="d-flex justify-content-between flex-lg-wrap">
                                                      <p class="text-dark fs-5 fw-bold mb-0">{{$product->price}}</p>
                                                      <a href="{{route('product.show', $product->uuid)}}" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
